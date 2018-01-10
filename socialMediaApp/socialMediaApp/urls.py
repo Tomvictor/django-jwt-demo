c99b57548bed91cd4bc7b880d634445e4d33a521
Tom Victor <vjtomvictor@icloud.com>
2018-01-10 20:33:47 +0530

    changes

--- socialMediaApp/socialMediaApp/urls.py
+++ socialMediaApp/socialMediaApp/urls.py
@@ -22,6 +22,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('get-token/', obtain_jwt_token),
-    path('api/',include('mainapp.urls'),namespace="mainapp")
+    path('api/',include('mainapp.urls',namespace="mainapp"))
 ]
 
